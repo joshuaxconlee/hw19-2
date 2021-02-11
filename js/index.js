61469f2f22b7d3d0c2c8febef8b0b1251a25e1b7
joshuaxconlee <joshconlee1@gmail.com>
2021-02-10 20:16:56 -0600

    committing to git

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var navbar = document.querySelector(".navBar")
+var ham = document.querySelector(".ham")
+
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+ham.addEventListener("click", toggleHamburger)
+
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
